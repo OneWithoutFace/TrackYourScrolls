611e28198088ff40dc80bed978dce429744ed781
Mick <mike.leduc@hotmail.ca>
2024-04-15 19:55:12 -0400

    Struc of menu done
    Signed-off-by: Mick <mike.leduc@hotmail.ca>

--- app/src/main/java/com/example/trackyourscrolls/MainActivity.java
+++ app/src/main/java/com/example/trackyourscrolls/MainActivity.java
@@ -4,12 +4,25 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.Dialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     ArrayList<ZoneModel>zoneModels = new ArrayList<>();
+    Button profileSheet;
+    ImageButton menuSheet;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +32,44 @@ public class MainActivity extends AppCompatActivity {
         Zone_Adapter adapter = new Zone_Adapter(this, zoneModels);
         recyclerView.setAdapter(adapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        menuSheet = findViewById(R.id.btnMenu);
+        menuSheet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDialogmenuUnlog();
+            }
+        });
+
+    }
+
+    private void showDialogmenuUnlog(){
+        final Dialog dialog = new Dialog(this);
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        dialog.setContentView(R.layout.menu_layout_unlog);
+
+        LinearLayout layoutHome = dialog.findViewById(R.id.layoutHomeUL);
+        LinearLayout layoutZone = dialog.findViewById(R.id.layoutZoneUL);
+
+        layoutHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Home", Toast.LENGTH_SHORT).show();
+            }
+        });
+        
+        layoutZone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Zone", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        dialog.show();
+        dialog.getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT,ViewGroup.LayoutParams.WRAP_CONTENT);
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        dialog.getWindow().getAttributes().windowAnimations = R.style.DialogAnimation;
+        dialog.getWindow().setGravity(Gravity.TOP);
     }
 
     private void setupZoneModel(){
